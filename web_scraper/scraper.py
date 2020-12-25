78efdf36a7c6b4bc700a3db4b21b7f18a9e235aa
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-12-25 19:05:44 +0200

    finished

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import pprint
 from bs4 import BeautifulSoup
+import json
+
 
 #URL =' https://en.wikipedia.org/wiki/NASA'
 
@@ -16,10 +18,28 @@ def get_citations_needed_count(URL):
 print(get_citations_needed_count('https://en.wikipedia.org/wiki/NASA')) 
 
 def get_citations_needed_report(URL):
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    results = soup.find_all(title="Wikipedia:Citation needed")
-    result = results.find_all('p')
-    for i in result:
-        
-print( get_citations_needed_report('https://en.wikipedia.org/wiki/NASA'))    
+ 
+    page = requests.get(URL)
+    all_results = BeautifulSoup(page.content, 'html.parser').find_all('p')
+    result = ''
+    for i in all_results:
+        try:
+            if i.find_all('a', title='Wikipedia:Citation needed'):
+                for j in range(len(i.text)):
+                    if i.text[j] == '[' and i.text[j+16] == ']':
+                        dot_in = 0
+                        for jj in range(len(i.text[:j-5])):
+                            if i.text[:j][jj] == '.':
+                                dot_in = jj+2
+                        result += f'\n\nThis sentence need citation: {i.text[:j][dot_in:]}\nand the full article is:\n  {i.text}'
+        except:
+            continue
+    return result
+
+
+
+if __name__ == "__main__":
+    #print(get_citations_needed_count("https://en.wikipedia.org/wiki/NASA"))
+    print( get_citations_needed_report("https://en.wikipedia.org/wiki/NASA")) 
+    
+   
